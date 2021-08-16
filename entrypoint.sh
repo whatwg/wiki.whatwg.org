03fa195ae2ef9c1466c1bef789816069fec8ae9e
Domenic Denicola <d@domenic.me>
2021-08-16 14:03:47 -0400

    Run the update script in the background
    This should hopefully fix the recurring 15-minute downtimes. The hypothesis is that DigitalOcean is re-spinning the VM and running the Dockerfile each time, and gets confused when the server isn't running immediately.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 # Perform any database updates since the last deply.
 # --quick means "skip the five-second countdown".
-php /var/www/html/maintenance/update.php --quick
+php /var/www/html/maintenance/update.php --quick &
 
 docker-php-entrypoint apache2-foreground
